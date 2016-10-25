58dd2473af64b952c804ebe2a23673b88f1ce776
Ralph Janssen <ralphjanssen@gmail.com>
2016-10-25 13:10:50 +0200

    Testsuite files

--- source/index.js
+++ source/index.js
@@ -1,15 +1,16 @@
-export function alwaysTrue () {
-  return true
-}
-
-export function isEven (n) {
-  return n % 2 === 0
-}
-
-export function countLetters (word) {
-  if (typeof word !== 'string') {
-    return false
-  } else {
-    return Array.prototype.slice.call(word).length
-  }
-}
+// export function alwaysTrue () {
+//   return true
+// }
+//
+// export function isEven (n) {
+//   return n % 2 === 0
+// }
+//
+// export function countLetters (word) {
+//   if (typeof word !== 'string') {
+//     return false
+//   } else {
+//     return Array.prototype.slice.call(word).length
+//   }
+// }
+// // 
