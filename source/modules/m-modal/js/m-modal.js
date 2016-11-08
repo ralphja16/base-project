264fe8128ed27c7b3940a50d45359f1254604298
Ralph Janssen <ralphjanssen@gmail.com>
2016-11-08 13:05:26 +0100

    Update modal

--- source/modules/m-modal/js/m-modal.js
+++ source/modules/m-modal/js/m-modal.js
@@ -2,46 +2,36 @@
 
 let mModal = (function () {
 
-    let     holder          = document.querySelectorAll('.m-modal'),
+    let     holder          = document.querySelector('.m-modal'),
             body            = document.getElementsByTagName('body')[0],
-            modalButton     = document.querySelectorAll('.m-modal__trigger'),
-            modalsBg        = document.querySelectorAll('.m-modal__bg'),
-            modalcontent    = document.querySelectorAll('.m-modal__content'),
-            backdrop        = '<div class="m-modal__backdrop fade in"></div>',
+            modalButton     = document.querySelectorAll('.js--m-modal__trigger'),
+            span            = document.querySelectorAll('.m-modal__close'),
             len             = modalButton.length;
 
     var openModal = () => {
-        body.classList.add('m-modal--open');
-        holder.className += ' in';
-        holder.style.display = 'block';
-        body.innerHTML += backdrop;
-
+        holder.classList.add('is__open');
+        body.classList.remove('m-modal--open');
     },
-    closeModal = (e) => {
-        console.log('test');
-
-        // document.querySelector('.m-modal').addEventListener('click', (e) => {
-            if(e.target != modalcontent) {
-                document.querySelector('.m-modal').style.display = 'none';
-                document.querySelector('.m-modal').classList.remove('in');
-                body.classList.remove('m-modal--open');
-                document.querySelector('.m-modal__backdrop').remove();
-
-            }
-
-        // });
-
-
+    closeModal = () => {
+        holder.classList.remove('is__open');
+        body.classList.remove('m-modal--open');
     };
+
+    window.onclick = function(event) {
+        if (event.target == holder) {
+            holder.classList.remove('is__open');
+            body.classList.remove('m-modal--open');
+        }
+    };
+
     var bindActions = () => {
         for (var i = 0; i < len; i++) {
             modalButton[i].addEventListener('click', openModal, false);
-            modalsBg[i].addEventListener('click', closeModal, false);
-            // modalsBg[i].addEventListener('click', close, false);
+            span[i].addEventListener('click', closeModal, false);
         }
     };
     var init = () => {
-            bindActions();
+        bindActions();
     };
 
     return {
