d4fddab1cbe0f3629b130c5f2836e35adf0175ca
Ralph Janssen <ralphjanssen@gmail.com>
2016-11-19 22:42:08 +0100

    Update modals -> opens specific modal on click

--- source/modules/m-modal/js/m-modal.js
+++ source/modules/m-modal/js/m-modal.js
@@ -2,24 +2,31 @@
 
 let mModal = (function () {
 
-    let     holder          = document.querySelector('.m-modal'),
-            body            = document.getElementsByTagName('body')[0],
+    let     body            = document.getElementsByTagName('body')[0],
             modalButton     = document.querySelectorAll('.js--m-modal__trigger'),
             span            = document.querySelectorAll('.m-modal__close'),
             len             = modalButton.length;
 
-    var openModal = () => {
-        holder.classList.add('is__open');
+    var openModal = (e) => {
+        // get data-modal ID of clicked button
+        let _hashId = e.target.dataset.modal;
+
+        // strip data-modal from #
+        let _id = _hashId.substr(1);
+
+        // Add class to the modal with same ID as data-modal
+        document.getElementById(_id).classList.add('is__open');
         body.classList.remove('m-modal--open');
     },
     closeModal = () => {
-        holder.classList.remove('is__open');
+        document.querySelector('.is__open').classList.remove('is__open');
         body.classList.remove('m-modal--open');
     };
 
     window.onclick = function(event) {
-        if (event.target == holder) {
-            holder.classList.remove('is__open');
+
+        if (event.target == document.querySelector('.is__open')) {
+            document.querySelector('.is__open').classList.remove('is__open');
             body.classList.remove('m-modal--open');
         }
     };
