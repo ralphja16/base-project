9263ced739686241ea1a6fc7adbfe2ce4f1a4fda
Ralph Janssen <ralphjanssen@gmail.com>
2016-12-12 21:04:43 +0100

    Update tabs for mobile

--- source/modules/m-tabs/js/_m-tabs.js
+++ source/modules/m-tabs/js/_m-tabs.js
@@ -5,14 +5,14 @@ const mTabs = (function () {
     let holder          = document.querySelector('.m-tabs'),
         tabItem         = holder.querySelectorAll('.m-tabs__item__link'),
         tabPane         = holder.querySelectorAll('.m-tabs__pane'),
+        tabSelectbox    = holder.querySelector('.m-tabs__nav__selectbox'),
         activeIndex     = 0,
         initCalled      = false;
 
     let setup = () => {
-
         if (!initCalled) {
             initCalled = true;
-    
+
             for (var i = 0; i < tabItem.length; i++) {
                 var link = tabItem[i];
                 triggerClickListener(link, i);
@@ -22,6 +22,13 @@ const mTabs = (function () {
     triggerClickListener = (link, index) => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
+            goToTab(index);
+            tabSelectbox.selectedIndex = index;
+        });
+    },
+    goToTabMobile = () => {
+        tabSelectbox.addEventListener('change', () => {
+            let index = tabSelectbox.value;
             goToTab(index);
         });
     },
@@ -37,7 +44,7 @@ const mTabs = (function () {
 
     const init = () => {
         setup();
-        // triggerClickListener();
+        goToTabMobile();
     };
 
     return {
