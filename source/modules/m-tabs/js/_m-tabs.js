0da3da71855eeb862fa6aa1dcb36a0a88b62c78f
Ralph Janssen <ralphjanssen@gmail.com>
2016-12-12 13:08:06 +0100

    Tabs working => TODO: styling and mobile version

--- source/modules/m-tabs/js/_m-tabs.js
+++ source/modules/m-tabs/js/_m-tabs.js
@@ -2,31 +2,55 @@
 
 const mTabs = (function () {
 
-    const holder          = document.querySelector('.m-tabs');
-    const tab             = holder.querySelector(`.m-tabs__item`);
-    const tabPane         = holder.querySelector(`.m-tabs__pane`);
+    let holder          = document.querySelector('.m-tabs'),
+        tabItem         = holder.querySelectorAll('.m-tabs__item__link'),
+        tabPane         = holder.querySelectorAll('.m-tabs__pane'),
+        activeIndex     = 0,
+        initCalled      = false;
 
-    let triggerClickListener = (e) => {
-        tab.onclick = (e) => {
-            // holder.classList.toggle('is--open');
-            console.log(this);
-        };
-    };
+    let setup = () => {
+
+        if (!initCalled) {
+            initCalled = true;
+    
+            for (var i = 0; i < tabItem.length; i++) {
+                var link = tabItem[i];
+                triggerClickListener(link, i);
+            }
+        }
+    },
+    triggerClickListener = (link, index) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            goToTab(index);
+        });
+    },
+    goToTab = (index) => {
+        if (index !== activeIndex && index >= 0 && index <= tabItem.length) {
+            tabItem[activeIndex].parentNode.classList.remove('m-tabs__item--selected');
+            tabItem[index].parentNode.classList.add('m-tabs__item--selected');
+            tabPane[activeIndex].classList.remove('m-tabs__pane--active');
+            tabPane[index].classList.add('m-tabs__pane--active');
+            activeIndex = index;
+        }
+    }
 
     const init = () => {
-        triggerClickListener();
+        setup();
+        // triggerClickListener();
     };
 
     return {
         init: init,
+        goToTab: goToTab,
     };
 
-}());
 
+})();
 
 const callback = function(){
     // Handler when the DOM is fully loaded
-    mTabs.init();
+   mTabs.init();
 };
 
 if ( document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll) ) {
