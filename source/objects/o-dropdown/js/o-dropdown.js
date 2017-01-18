16ff84415066d7a5bdeea190cebde3b949195187
Ralph Janssen <ralphjanssen@gmail.com>
2017-01-18 12:06:36 +0100

    Update javascript dropdown and tooltip

--- source/objects/o-dropdown/js/o-dropdown.js
+++ source/objects/o-dropdown/js/o-dropdown.js
@@ -2,17 +2,21 @@
 
 const oDropdown = (function () {
 
-    let     holder          = document.querySelector('.o-dropdown'),
-            dropdownButton  = document.querySelector('.o-dropdown-toggle');
+    let     holder          = document.querySelectorAll('.o-dropdown'),
+            len             = holder.length;
 
-    let triggerClickListener = () => {
-        dropdownButton.onclick = () => {
-            holder.classList.toggle('is--open');
-        };
+    let triggerClickListener = (e) => {
+        e.target.parentNode.classList.toggle('is--open');
+    };
+
+    let bindActions = () => {
+        for (let i = 0; i < len; i++) {
+            holder[i].addEventListener('click', triggerClickListener, false);
+        }
     };
 
     const init = () => {
-        triggerClickListener();
+        bindActions();
     };
 
     return {
