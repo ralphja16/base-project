a24430673efb342cd5138bcf09bea588a693b12d
Ralph Janssen <ralphjanssen@gmail.com>
2018-01-01 22:13:23 +0100

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,38 @@
-var gulp            = require('gulp'),
-    config          = require('./gulp/config.js'),
-    requireDir      = require('require-dir');
+// package vars
+const pkg = require('./package.json');
+const config = require('./gulp/config.js');
+const gulp = require('gulp');
+const requireDir = require('require-dir');
 
-// Pulling in all tasks from the tasks folder
-requireDir('./gulp/tasks', { recurse: true });
-
-
-//GULP WATCH TASK
-gulp.task('watch', ['browser-sync'], function(){
-	gulp.watch(config.paths.sass.src2, ['sass']);
-	gulp.watch(config.paths.tests, ['mocha']);
-	gulp.watch(config.paths.scripts.src, ['scripts']);
-	gulp.watch(config.paths.svgIcons.src, ['sprite']).on('change', function(evt) {
-		changeEvent(evt);
-	});
+// load all plugins in 'devDependencies' into the variable $
+const $ = require('gulp-load-plugins')({
+	pattern: ['*'],
+	scope: ['devDependencies']
 });
 
-//GULP SETUP TASK
-gulp.task('setup', ['sass', 'scripts', 'vendors', 'images']);
+// // Pulling in all tasks from the tasks folder
+requireDir('./gulp/tasks', { recurse: true });
 
-//GULP BUILD TASK
-gulp.task('build', ['watch', 'browser-sync']);
+//GULP WATCH TASK
+// gulp.task('watch', ['browser-sync'], function() {
+// 	gulp.watch(config.paths.sass.src2, ['sass']);
+// 	gulp.watch(config.paths.tests, ['mocha']);
+// 	gulp.watch(config.paths.scripts.src, ['scripts']);
+// 	gulp
+// 		.watch(config.paths.svgIcons.src, ['sprite'])
+// 		.on('change', function(evt) {
+// 			changeEvent(evt);
+// 		});
+// });
 
-//DEFAULT GULP TASK
-gulp.task('default', ['watch', 'browser-sync']);
+// //GULP SETUP TASK
+// gulp.task('setup', ['sass', 'scripts', 'vendors', 'images']);
 
-//DEPLOY GULP TASK
-gulp.task('deploy', ['sass_deploy', 'scripts_deploy', 'vendors', 'images']);
+// //GULP BUILD TASK
+// gulp.task('build', ['watch', 'browser-sync']);
+
+// //DEFAULT GULP TASK
+// gulp.task('default', ['watch', 'browser-sync']);
+
+// //DEPLOY GULP TASK
+// gulp.task('deploy', ['sass_deploy', 'scripts_deploy', 'vendors', 'images']);
