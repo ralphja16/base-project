f18b701b8d276b156db621736f463f8633ee4c4f
Ralph Janssen <ralphjanssen@gmail.com>
2018-01-05 09:31:26 +0100

    update caret animation

--- source/modules/m-navigation/js/m-navigation.js
+++ source/modules/m-navigation/js/m-navigation.js
@@ -1,100 +1,57 @@
-(function(window) {
-	'use strict';
+class Navigation {
+	constructor(holder) {
+		this.holder = document.querySelector(holder);
+		this.navItems = this.holder.querySelectorAll('.m-navigation__item');
+		this.toggleButton = this.holder.querySelector('.m-navigation__toggle');
+		this.activeClass = 'is--active';
+		this.initEventHandler();
+		this.mobileToggle(this.holder);
+	}
 
-	class Navigation {
-		constructor(holder) {
-			this.holder = holder;
-			this.navItems = document.querySelectorAll(
-				'.m-navigation .m-navigation__item'
-			);
+	show(item) {
+		item.classList.add(this.activeClass);
+	}
 
-			this.activeClass = 'is--active';
-			this.initEventHandler();
-		}
+	hide(item) {
+		item.classList.remove(this.activeClass);
+	}
 
-		show(item) {
-			item.classList.add(this.activeClass);
-		}
+	mobileToggle(holder) {
+		this.toggleButton.addEventListener('click', function() {
+			const iconElement = this.firstElementChild;
+			const bars = 'fa-bars';
+			const cross = 'fa-times';
 
-		hide(item) {
-			item.classList.remove(this.activeClass);
-		}
+			if (iconElement.classList.contains(bars)) {
+				iconElement.classList.add(cross);
+				iconElement.classList.remove(bars);
+			} else {
+				iconElement.classList.add(bars);
+				iconElement.classList.remove(cross);
+			}
+			holder.classList.toggle('is--open');
+		});
+	}
 
-		initEventHandler() {
-			const _this = this;
+	initEventHandler() {
+		const _this = this;
 
-			for (let i = 0; i < this.navItems.length; i++) {
-				const item = this.navItems[i];
+		for (let i = 0; i < this.navItems.length; i++) {
+			const item = this.navItems[i];
 
-				if (item.querySelector('.m-navigation__submenu')) {
-					item.addEventListener('click', function(e) {
-						e.preventDefault();
+			if (item.querySelector('.m-navigation__submenu')) {
+				item.addEventListener('click', function(e) {
+					e.preventDefault();
 
-						if (this.classList.contains(_this.activeClass)) {
-							_this.hide(this);
-						} else {
-							_this.show(this);
-						}
-					});
-				}
+					if (this.classList.contains(_this.activeClass)) {
+						_this.hide(this);
+					} else {
+						_this.show(this);
+					}
+				});
 			}
 		}
 	}
+}
 
-	window.Navigation = Navigation;
-})(window);
-
-const holder = document.querySelectorAll('.m-navigation');
-const navigation = new Navigation(holder);
-
-// (function(window) {
-// 	'use strict';
-
-// 	const Navigation = function(holder) {
-// 		var _this = this;
-
-// 		const init = () => {
-// 			console.log(holder);
-// 			console.log('test');
-// 			this.clickListener();
-// 		};
-
-// 		this.clickListener = () => {};
-
-// 		return {
-// 			init: init
-// 		};
-// 	};
-
-// 	const callback = function() {
-// 		// Handler when the DOM is fully loaded
-// 		window.Navigation = Navigation().init();
-// 		console.log(window);
-// 	};
-
-// 	if (
-// 		document.readyState === 'complete' ||
-// 		(document.readyState !== 'loading' &&
-// 			!document.documentElement.doScroll)
-// 	) {
-// 		callback();
-// 	} else {
-// 		document.addEventListener('DOMContentLoaded', callback);
-// 	}
-// })(window);
-
-// const callback = function() {
-// 	// Handler when the DOM is fully loaded
-// 	var mNavigation = new Navigation('.m-navigation');
-// };
-
-// if (
-// 	document.readyState === 'complete' ||
-// 	(document.readyState !== 'loading' && !document.documentElement.doScroll)
-// ) {
-// 	callback();
-// } else {
-// 	document.addEventListener('DOMContentLoaded', callback);
-// }
-
-// export default Navigation;
+const navigation = new Navigation('.m-navigation');
