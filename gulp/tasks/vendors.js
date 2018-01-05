745d3ec33844f08682dc1229f981fba17774b8ec
Ralph Janssen <ralphjanssen@gmail.com>
2018-01-05 09:26:03 +0100

    Update gulp vendors task

--- gulp/tasks/vendors.js
+++ gulp/tasks/vendors.js
@@ -1,49 +1,57 @@
 const gulp = require('gulp');
 const config = require('../config.js');
+const onError = require('../errorHandler');
+const pkg = require('../../package.json');
 const fancyLog = require('fancy-log');
 const plumber = require('gulp-plumber');
 const size = require('gulp-size');
-const filter = require('gulp-filter');
 const gulpif = require('gulp-if');
-const livereload = require('gulp-livereload');
 const newer = require('gulp-newer');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
-const babel = require('babel-core');
+const concat = require('gulp-concat');
+const moment = require('moment');
+const gitRevSync = require('git-rev-sync');
+const header = require('gulp-header');
 
-const onError = err => {
-	console.log(err);
-};
-
-// const banner = [
-//     "/**",
-//     " * @project        <%= pkg.name %>",
-//     " * @author         <%= pkg.author %>",
-//     " * @build          " + moment().format("llll") + " ET",
-//     " * @release        " + gitRevSync.long() + " [" + $.gitRevSync.branch() + "]",
-//     " * @copyright      Copyright (c) " + moment().format("YYYY") + ", <%= pkg.copyright %>",
-//     " *",
-//     " */",
-//     ""
-// ].join("\n");
+const banner = [
+	'/**',
+	' * @project        <%= pkg.name %>',
+	' * @author         <%= pkg.author %>',
+	' * @build          ' + moment().format('llll') + ' ET',
+	' * @release        ' +
+		gitRevSync.long() +
+		' [' +
+		gitRevSync.branch() +
+		']',
+	' * @copyright      Copyright (c) ' +
+		moment().format('YYYY') +
+		', <%= pkg.copyright %>',
+	' *',
+	' */',
+	''
+].join('\n');
 
 // js task - minimize any distribution Javascript into the public js folder, and add our banner to it
 gulp.task('vendors', () => {
 	fancyLog('-> Building js');
-	return (
-		gulp
-			.src(config.paths.vendors.src)
-			.pipe(plumber({ errorHandler: onError }))
-			// .pipe(gulpif(["*.js", "!*.min.js"],
-			//     newer({dest: config.paths.vendors.dest, ext: ".min.js"}),
-			//     newer({dest: config.paths.vendors.dest.js})
-			// ))
-			.pipe(gulpif(['*.js', '!*.min.js'], uglify()))
-			.pipe(gulpif(['*.js', '!*.min.js'], rename({ suffix: '.min' })))
-			// .pipe($.header(banner, {pkg: pkg}))
-			.pipe(size({ gzip: true, showFiles: true }))
-			.pipe(gulp.dest(config.paths.vendors.dest))
-			.pipe(filter('**/*.js'))
-			.pipe(livereload())
-	);
+	return gulp
+		.src(config.paths.vendors.src)
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(
+			gulpif(
+				['*.js', '!*.min.js'],
+				newer({
+					dest: config.paths.vendors.dest,
+					ext: '.min.js'
+				}),
+				newer({ dest: config.paths.vendors.dest })
+			)
+		)
+		.pipe(concat('vendors.js'))
+		.pipe(gulpif(['*.js', '!*.min.js'], uglify()))
+		.pipe(gulpif(['*.js', '!*.min.js'], rename({ suffix: '.min' })))
+		.pipe(header(banner, { pkg: pkg }))
+		.pipe(size({ gzip: true, showFiles: true }))
+		.pipe(gulp.dest(config.paths.vendors.dest));
 });
