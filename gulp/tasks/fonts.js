3fef25a3d4bcfda9e541e2db4e56cc75a7b802f0
Ralph Janssen <ralph@hellosunshine.nl>
2018-03-10 08:57:59 +0100

    Fixing copy fonts gulp task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -3,6 +3,6 @@ const config = require('../config.js');
 
 gulp.task('fonts', () => {
 	return gulp
-		.src(config.paths.fonts.src)
+		.src(config.paths.fonts.src, { base: './src/assets/fonts' })
 		.pipe(gulp.dest(config.paths.fonts.dest));
 });
