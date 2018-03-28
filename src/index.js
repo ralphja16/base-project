3e7a68556d38e3834dd376b3e6661e3770d16eb0
Ralph Janssen <ralphjanssen@gmail.com>
2018-03-28 12:43:17 +0200

    Small fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './index.scss';
+import { Table } from './components/objects/index';
+import Slider from './components/modules/m-slider/js/m-slider.jquery';
 
 function calc(a, b) {
 	return a + b;
 }
 
-alert(calc(2, 4));
+// alert(calc(2, 4));
